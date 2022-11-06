ccccacc196809f2afa1efeca8efdb2cea2c7ce59
warspyt <tatianax2442@gmail.com>
2022-11-05 21:49:13 -0500

    base interfaz hasta picture

--- app/src/main/java/com/example/remind_app/MenuPrincipal.java
+++ app/src/main/java/com/example/remind_app/MenuPrincipal.java
@@ -1,6 +1,8 @@
 package com.example.remind_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,11 @@ public class MenuPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
         getSupportActionBar().hide();
+    }
+
+    /** Funcion para ingresar al juego picture */
+    public void IngresoPicture(View view) {
+        Intent pictureGame = new Intent (this, Picture.class);
+        startActivity(pictureGame);
     }
 }
